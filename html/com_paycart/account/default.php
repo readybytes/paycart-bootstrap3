dff676219ef31ce96eab105466e509476ea56499
rbsl-rakesh <rakesh@readybytes.in>
2015-10-19 12:32:53 +0530

    improve::change layout of My orders page in account section

--- html/com_paycart/account/default.php
+++ html/com_paycart/account/default.php
@@ -14,8 +14,8 @@ defined( '_JEXEC' ) or die( 'Restricted access' );
 echo $this->loadTemplate('css');
 echo $this->loadTemplate('js');
 ?>
-<div class='pc-account-wrapper row clearfix'> 
-	<div id="pc-account" class ='pc-account col-sm-12 clearfix' >
+<div class='pc-account-wrapper'> 
+	<div id="pc-account" class ='pc-account' >
 	
 		<!-- START HEADER -->
 		<div class="pc-account-header hidden-xs">
@@ -26,59 +26,114 @@ echo $this->loadTemplate('js');
 		
 		<!-- START BODY -->
 		<div class="row">
-			<div class="col-sm-3 hidden-xs">
+			<div class="col-md-3 hidden-xs">
 				<?php echo $this->loadTemplate('sidebar');?>
 			</div>
-			<div class="col-sm-9 col-xs-12">
-				<div class ='pc-account clearfix' >
-				<table class="table table-hover">
-					<thead>					
-						<tr>
-							<th width="10%">#</th>
-							<th><?php echo JText::_('COM_PAYCART_ORDERS');?> <span class="badge"><?php echo $total_orders;?></span></th>
-						</tr>
-					</thead>
-					<tbody>
-						<?php $counter = $limitstart+1;?>
+			<div class="col-md-9">
+				<div class ='pc-account clearfix' >						
+						<div class="all-orders">
+							<h2 class="page-header"><?php echo JText::_('COM_PAYCART_ORDERS');?> <sup><span class="badge"><?php echo $total_orders;?></span></sup></h2>	
+						</div>					
 						<?php foreach($carts as $cart_id => $cart): ?>
-							<tr class="pc-account-orders" data-pc-cart-url="<?php echo JRoute::_('index.php?option=com_paycart&view=account&task=order&order_id='.$cart_id);?>">
-								<td><?php echo $counter++;?></td>
-								<td> 
-									<div><h3><a href="<?php echo JRoute::_('index.php?option=com_paycart&view=account&task=order&order_id='.$cart_id);?>"><?php echo JText::_('COM_PAYCART_ORDER_ID');?> : <?php echo $cart->cart_id;?></a></h3></div>
-									<div>
-										<h4>
-											<?php echo JText::_('COM_PAYCART_AMOUNT');?> : <?php echo $formatter->amount($invoices[$cart->invoice_id]['total']);?>
-											<?php if($cart->status === Paycart::STATUS_CART_PAID):?>
-												<i class="fa fa-check-circle text-success" title="<?php echo JText::_('COM_PAYCART_CART_STATUS_PAID');?>"></i>
-											<?php endif;?>
-										</h4>										
+							<?php 
+								$params = json_decode($cart->params);	
+								$products = get_object_vars($params->products);
+							?>
+							<div class="order-items-wraper accordion" id="accordion-order<?php echo $cart_id;?>">
+								<div class="panel-group">
+									<div class="panel panel-default">
+										<div class="accordion-header well-sm panel-heading">
+											<div class="accordion-toggle">
+												<div class="row"  data-toggle="collapse" data-parent="#accordion-order<?php echo $cart_id;?>" data-target="#order_<?php echo $cart_id?>">
+													<div class="col-sm-6">
+														<div class="row">
+															<div class="col-sm-8">
+																<h3 class="order-item-title font500 clearfix">
+																	<span class="pull-left"><?php echo JText::_('COM_PAYCART_ORDER_ID');?> : <?php echo $cart->cart_id;?></span>  
+																	<span class="text-muted pull-left"><small>[<?php echo count($products);?> <?php echo JText::_('COM_PAYCART_ITEM');?>(s)]</small></span>
+																</h3>
+																<span class="text-muted"><?php echo JText::_('COM_PAYCART_ORDER_PLACED_ON');?> : <?php echo $formatter->date(new Rb_Date($cart->created_date));?></span>
+															</div>
+															<div class="col-sm-4">
+																<h4 class="font500"><?php echo JText::_('COM_PAYCART_TOTAL');?> <?php echo JText::_('COM_PAYCART_AMOUNT');?> </h4> 
+																<span class="text-muted"><?php echo $formatter->amount($invoices[$cart->invoice_id]['total']);?></span>
+															</div>
+														</div>
+													</div>
+													<div class="col-sm-6">
+														<div class="pull-right" style="margin-right:20px">
+															<h4 class="font500 clearfix">
+																<a class="pull-right view-order-details" href="<?php echo JRoute::_('index.php?option=com_paycart&view=account&task=order&order_id='.$cart_id);?>"><?php echo JText::_('COM_PAYCART_VIEW_ORDER_DETAILS');?> <i class="fa fa-angle-double-right"></i></a>
+															</h4>
+														</div>
+													</div>
+												</div>
+											</div>
+										</div>
+										<div class="order-items-content">
+											<div id="order_<?php echo $cart_id?>" class="accordion-body panel-collapse collapse in">
+											<?php
+																
+											
+											foreach($products as $product_id => $product) :
+												$product_instance = PaycartProduct::getInstance($product_id);
+												$product_price = $product_instance->getPrice();
+												$product_quantity = $product_instance->getQuantitySold();
+												$image = $product_instance->getCoverMedia();
+											?>
+												
+												<div class="accordion-inner panel-body">
+													<div class="clearfix">
+														<?php if($cart->isShippableProductExist):?>
+																<?php if($cart->is_delivered) :?>
+																		<h4 class="font500"><?php echo JText::_('COM_PAYCART_DELIVERY_STATUS');?> : <span class="label label-success"><?php echo JText::_('COM_PAYCART_CART_STATUS_DELIVERED');?></span></h4>
+																		<h4 class="font500"><?php echo JText::_('COM_PAYCART_CART_STATUS_DELIVERED');?> <?php echo strtolower(JText::_('COM_PAYCART_ON'));?>: <?php echo $formatter->date(new Rb_Date($cart->delivered_date));?></h4>
+																<?php else :?>
+																		<h4 class="font500"><?php echo JText::_('COM_PAYCART_DELIVERY_STATUS');?> : <span class="label label-warning"><?php echo JText::_('COM_PAYCART_CART_STATUS_PENDING');?></span></h4>												
+																<?php endif;?>
+														<?php endif;?>									
+													</div>
+													
+													<div class="row">
+														<div class="col-sm-2">
+															<a href="<?php echo JRoute::_('index.php?option=com_paycart&view=product&task=display&product_id='.$product_id);?>" class="thumbnail">
+																<img src="<?php echo !empty($image) ? $image['thumbnail'] : '';?>" 
+																	title="<?php echo !empty($image) ? $image['title'] : '';?>" 
+																	alt="<?php echo !empty($image) ? $image['title'] : '';?>" />
+															</a>
+														</div>
+														<div class="col-sm-10">
+															<h4 class="font500"><a href="<?php echo JRoute::_('index.php?option=com_paycart&view=product&task=display&product_id='.$product_id);?>"><?php echo $product_instance->getTitle();?></a></h4>
+															<h5 class="ordered-item-details"><span class="text-muted"><?php echo JText::_('COM_PAYCART_QTY');?> : <?php echo $product_quantity;?></span></h5>
+															<h5 class="ordered-item-details"><span class="text-muted"><?php echo JText::_('COM_PAYCART_PRICE');?> : </span><span class="text-error"><?php echo $formatter->amount($product_price);?></span></h5>
+														</div>
+													</div>
+												</div>
+											
+											<?php endforeach;?>
+												
+											<?php 
+											//Invoice Link	
+											$downloadUrl = JRoute::_('index.php?option=com_paycart&view=pdfdownload&task=sitedownload&action=sitePdfDownload&cart_id='.$cart_id);
+											if(Rb_HelperPlugin::getStatus('pdfdownload','paycart')):?>
+												<div class="clearfix well-sm">
+														<span class="pull-right btn-link view-order-details download-invoice" onclick="rb.url.redirect('<?php echo $downloadUrl; ?>');  event.stopPropagation(); ">
+															 <i class="fa fa-1x fa-file-pdf-o"></i> <?php echo JText::_('COM_PAYCART_INVOICE');?>
+														</span>
+												</div>
+											<?php endif;?>											
+											</div>
+										</div>
 									</div>
-									<div><?php echo JText::_('COM_PAYCART_CREATED_DATE');?> : <?php echo $formatter->date(new Rb_Date($cart->created_date));?></div>
-									<div><?php echo JText::_('COM_PAYCART_STATUS');?> :
-										<?php if($cart->is_delivered) :?>
-												<span class="label label-success"><?php echo JText::_('COM_PAYCART_CART_STATUS_DELIVERED');?></span>
-												<?php echo strtolower(JText::_('COM_PAYCART_ON'));?>
-												<?php echo $formatter->date(new Rb_Date($cart->delivered_date));?>
-										<?php else :?>
-												<span class="label label-warning"><?php echo JText::_('COM_PAYCART_CART_STATUS_PENDING');?></span>												
-										<?php endif;?>
-									</div>
-								</td>
-							</tr>
-						<?php endforeach;?>
-					</tbody>
-					<tfoot>
-						<tr>
-							<td colspan="2">
-								<div class="center">
-									<?php echo $pagination->getListFooter(); ?>
-								</div>								
-							</td>
-						</tr>
-					</tfoot>
-				</table>
+								</div>
+							</div>
+							<?php endforeach;?>
+							<div class="center">
+								<?php echo $pagination->getListFooter(); ?>
+							</div>
 				</div>				
 			</div>
 		</div>      	
 	</div>
 </div>
+<?php 
--- html/com_paycart/account/default_css.php
+++ html/com_paycart/account/default_css.php
@@ -11,78 +11,38 @@
 defined( '_JEXEC' ) or die( 'Restricted access' );
 ?>
 <style>
-.pc-account-header-avatar{
-	margin-bottom: 10px;
-    margin-right: 10px;
-}
-.pc-account-header-name{
-    margin-top: 0;
-}
-
-.pc-account-orders{
-	cursor: pointer;
-}
-
-.pc-account-order-details{
-	border : 1px solid #E5E5E5;
-	padding: 10px;
-	margin-bottom: 30px;
-}
-
-.pc-account .heading{
-	font-size: 16px;
-    font-weight: bold;
-}
-
-.pc-account-order-product .table .progress{
-	height: 8px;
-	margin-bottom: 10px;
-}
-
-.pc-track-tbl-border{
-	 padding-top: 5px;
-}
-
-.shipment-row .table th, .shipment-row .table td {
-    border-top: medium none;
-}
-
-.pc-track-shipment .table td {
-    width: 50%;
-}
-
-.pc-track-note {
-    padding: 8px;
-}
-
-.pc-track-arrow::after {
-    border-color: #e3e3e3 transparent;
-    border-style: solid;
-    border-width: 0 15px 15px;
-    content: "";
-    display: block;
-    left: 50%;
-    margin-left: -15px;
-    position: absolute;
-    top: -15px;
-    width: 0;
-    z-index: 1;
-}
-
-.pc-track-shipment .well {
-    padding: 10px;
-}
-
-.pc-track-arrow {
-    position: relative;
-}
-
-.pc-account-order-productdetails-header .heading {
-    line-height: 2;
-    font-weight: 600;
-    color: #777;
-}
+.pc-account-header-avatar{margin-bottom: 10px;margin-right: 10px;}
+.pc-account-header-name{margin-top: 0;}
+.pc-account-orders{cursor: pointer;}
+.pc-account-order-details{border : 1px solid #E5E5E5;padding: 10px;margin-bottom: 30px;}
+.pc-account .heading{font-size: 16px;font-weight: bold;}
+.pc-account-order-product .table .progress{height: 8px;margin-bottom: 10px;}
+.pc-track-tbl-border{padding-top: 5px;}
+.shipment-row .table th, .shipment-row .table td {border-top: medium none;}
+.pc-track-shipment .table td {width: 50%;}
+.pc-track-note {padding: 8px;}
+.pc-track-arrow::after {border-color: #e3e3e3 transparent;border-style: solid;border-width: 0 15px 15px;content: "";display: block;left: 50%;margin-left: -15px;position: absolute;top: -15px;width: 0;z-index: 1;}
+.pc-track-shipment .well {padding: 10px;}
+.pc-track-arrow {position: relative;}
+.pc-account-order-productdetails-header .heading {line-height: 2;font-weight: 600;color: #777;}
 .shipment-row .dwn-icon{font-size: 16px;color: #333;}
-
+.accordion-inner {padding: 9px 15px;border-top: 1px solid #e5e5e5;}
+.pc-account-order-productdetails {border-color: -moz-use-text-color #eee #eee;border-style: none solid solid;border-width: 0 1px 1px;}
+.all-orders {font-weight: 600;font-size: 18px;}
+.accordion-group {background-color: #fff;border: 1px solid #ddd;border-radius: 4px;-webkit-box-shadow: 0 1px 1px rgba(0,0,0,.05);box-shadow: 0 1px 1px rgba(0,0,0,.05);}
+.accordion-toggle { cursor: pointer;}
+.accordion-header.well-small {color: #333;background-color: #f5f5f5;border-color: #ddd;}
+.font500 {font-weight: 500;}
+.order-item-title > span {margin-right: 6px;}
+.order-items-content .well-sm {border-top: 1px solid #eee;}
+.view-order-details {color: #333;}
+a.thumbnail:hover{text-decoration:none;border-color: #ddd;}
+.pc-account .h3,.pc-account h3{font-size: 18px;line-height: 20px;}
+.pc-account .h4,.pc-account h4 {font-size: 14px;line-height: 16px;}
+h1, h2, h3, h4, h5, h6 {margin: 12px 0;}
+.download-invoice {cursor:pointer;}
+@media (max-width:991px){
+.pc-account-header + .nav-stacked > li {display: inline-block;}
+}
 </style>
 <?php 
